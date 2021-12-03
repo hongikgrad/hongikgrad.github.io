0405f9b375a5acac83e64ea4f352342d64ac5eb9
junhwan <ujunhwan@gmail.com>
2021-12-04 01:17:08 +0900

    [feat] axios CORS 설정

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,17 @@ import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
+import axios from "axios";
 
 const store = createStore(
   rootReducer,
   (window as any).__REDUX_STATE__,
   composeWithDevTools()
 );
+
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 
 ReactDOM.render(
   <Provider store={store}>
