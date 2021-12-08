87c8803786100afcea0802e1d3a62846f1e04fc7
junhwan <ujunhwan@gmail.com>
2021-12-08 18:33:41 +0900

    [feat] 폰트 적용

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -4,7 +4,7 @@ const GlobalStyles = createGlobalStyle`
 body {
   margin: 0;
   padding: 0;
-  font-family: Noto Sans KR, GothamSSmBook, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", "Noto Sans CJK KR", arial, 돋움, Dotum, Tahoma, Geneva, sans-serif;
+  font-family: Montserrat, Noto Sans KR, GothamSSmBook, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", "Noto Sans CJK KR", arial, 돋움, Dotum, Tahoma, Geneva, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #212529;
@@ -32,25 +32,6 @@ a {
   -webkit-tap-highlight-color : transparent;
   text-decoration:none
 }
-
-@font-face {
-  font-family:GothamXLight;
-  src:url('./styles/Gotham-XLight.woff')
-}
-
-@font-face {
-  font-family: GothamSSmBook;
-  src: url('./styles/GothamSSm-Book.woff');
-}
-
-@font-face {
-  font-family: GothamSSmBookItalic;
-  src: url('./styles/GothamSSm-BookItalic.woff');
-}
-
-@font-face {font-family: 'Noto Sans KR';font-style: normal;font-weight: 400;src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2) format('woff2'),url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff) format('woff'),url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.otf) format('opentype');}
-
-
 
 `;
 
