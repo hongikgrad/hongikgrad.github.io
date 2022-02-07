b582928e9396101e851e4cd62f7f0451485915b5
junhwan <ujunhwan@gmail.com>
2022-02-07 23:47:11 +0900

    [feat] logout button 생성

---
+++ src/components/button/LogoutButton.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../modules/auth";
+import Button from "./Button";
+import TextButton from "./TextButton";
+
+export default function LogoutButton(props: any) {
+  const dispatch = useDispatch();
+  const handlerLogout = () => {
+    // dispatch(logoutUser());
+    window.sessionStorage.setItem("login", "false");
+  };
+  return (
+    <TextButton onClick={handlerLogout} width={1} height={1}>
+      logout
+    </TextButton>
+  );
+}
