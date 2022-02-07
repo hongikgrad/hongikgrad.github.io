9784e861cad45b0425651aed9fadf99ea746bdc8
junhwan <ujunhwan@gmail.com>
2022-02-07 23:57:59 +0900

    delete

--- src/pages/FeedbackPage.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import PageTemplate from "../components/PageTemplate";
-
-export interface FeedbackPageProps {}
-
-export default function FeedbackPage(props: FeedbackPageProps) {
-  return <PageTemplate></PageTemplate>;
-}
