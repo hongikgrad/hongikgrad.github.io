568f426a8a217a95351646944e8291665a86901e
junhwan <ujunhwan@gmail.com>
2022-02-07 23:39:42 +0900

    [feat] table component 추가

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledTable = styled.table`
+  width: 27rem;
+  border-collapse: collapse;
+
+  th {
+    // border-top: 1px solid black;
+    // border-bottom: 1px solid black;
+    border-top: 0.1rem solid black;
+    border-bottom: 0.1rem solid black;
+  }
+
+  td {
+    width: 2rem;
+    padding: 0.3rem;
+    // border-bottom: 1px solid black;
+    border-bottom: 0.1rem solid black;
+    a:link {
+      color: #333;
+    }
+    a:visited {
+      color: #c0c0c0;
+    }
+    a:hover {
+      color: #8b00ff;
+    }
+    a:active {
+      color: #8b00ff;
+    }
+  }
+
+  tbody {
+    width: 25rem;
+  }
+  tr {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export default function Table(props: any) {
+  const rows = props.rows;
+  const columns = props.columns;
+
+  return (
+    <StyledTable>
+      <thead>
+        <tr>
+          {columns.map((column: any, index: number) => (
+            <th key={index}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row: any) => {
+          const year =
+            typeof row.semester === "string"
+              ? row.semester.substring(0, 4)
+              : "";
+          const hakgi =
+            typeof row.semester === "string" ? row.semester.substring(4) : "";
+          const url =
+            typeof row.semester === "string"
+              ? `https://sugang.hongik.ac.kr/cn13061.jsp?yy=${year}&hakgi=${hakgi}&haksu=${row.number}&bunban=1`
+              : "/";
+          return (
+            <tr key={row.number + row.credit}>
+              <td>
+                <a
+                  key={row}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  {row.name}
+                </a>
+              </td>
+
+              <td>{row.number}</td>
+              <td>{row.credit}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </StyledTable>
+  );
+}
