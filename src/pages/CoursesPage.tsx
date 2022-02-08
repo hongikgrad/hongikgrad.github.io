ab54cfee1eb9ae3f26150eee9ab1ec13025a6e44
junhwan <ujunhwan@gmail.com>
2022-02-09 02:21:53 +0900

    [feat] course page tip 추가 및 검색 방법 변경

--- src/pages/CoursesPage.tsx
+++ src/pages/CoursesPage.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as config from "../config";
-import React, { useEffect, useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import PageTemplate from "../components/PageTemplate";
 import { Stack } from "@mui/material";
 
@@ -8,6 +8,7 @@ import SearchInput from "../components/input/SearchInput";
 import SearchButton from "../components/button/SearchButton";
 import Table from "../components/table/Table";
 import MoreButton from "../components/button/MoreButton";
+import Divider from "../components/Divider";
 
 const BASE_URL = config.API_BASE_URL as string;
 
@@ -30,6 +31,39 @@ type CourseListType = {
   [key: number]: CourseType;
 };
 
+const bold = {
+  fontWeight: 700,
+};
+
+type TipType = {
+  tipTab: boolean;
+};
+
+function Tip(props: TipType) {
+  return (
+    <>
+      {props.tipTab && (
+        <>
+          <Divider />
+          <br />
+          <div>
+            <span style={bold}>카테고리</span> 검색
+          </div>
+          <div>ex) cat:사회, cat:전산</div>
+          <br />
+          <div>
+            <span style={bold}>전공</span> 검색
+          </div>
+          <div>ex) major:전자, major:컴퓨터 </div>
+          <br />
+          <Divider />
+          <br />
+        </>
+      )}
+    </>
+  );
+}
+
 export default function CoursesPage(Props: Props) {
   const [rows, setRows] = useState<Array<CourseListType>>([]);
   const [totalCount, setTotalCount] = useState<number>(0);
@@ -39,9 +73,14 @@ export default function CoursesPage(Props: Props) {
   const [searchInput, setSearchInput] = useState<string>("");
   const [hasSearch, setHasSearch] = useState<boolean>(false);
   const [searchCount, setSearchCount] = useState<number>(0);
+  const [tipTab, setTipTab] = useState<boolean>(true);
 
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleTipTab = () => {
+    setTipTab(!tipTab);
+  };
 
   const handleChangePage = (event: any, newPage: number) => {
     setPage(newPage);
@@ -60,7 +99,11 @@ export default function CoursesPage(Props: Props) {
     const config = {
       withCredentials: true,
     };
-    const url = BASE_URL + `/courses?keyword=${searchInput}`;
+    const [cmd, keyword] = searchInput.split(":");
+    const url =
+      keyword == null
+        ? BASE_URL + `/courses?keyword=${searchInput}`
+        : BASE_URL + `/courses?keyword=${keyword}&command=${cmd}`;
     axios.get(url, config).then((res) => {
       const courseList = res.data.courses;
       setRows([...courseList]);
@@ -70,27 +113,20 @@ export default function CoursesPage(Props: Props) {
     });
   };
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const url = `${BASE_URL}/courses/count`;
     axios.get(url).then((res) => {
       setTotalCount(res.data);
     });
   }, []);
 
-  useEffect(() => {
-    const url = `${BASE_URL}/courses?size=${rowsPerPage}&page=${page}`;
-    axios.get(url).then((res) => {
-      const courseList = res.data.courses;
-      setRows([...courseList]);
-      setRowLength(courseList.length);
-    });
-  }, [page, rowsPerPage]);
-
   const defaultNotice = `현재 ${totalCount}개의 수업이 등록되어 있습니다.`;
   const searchNotice = `${searchCount}개의 수업이 검색 되었습니다.`;
   return (
     <>
       <PageTemplate>
+        <h2 onClick={handleTipTab}>검색 방법</h2>
+        <Tip tipTab={tipTab} />
         <Stack
           direction="column"
           spacing={2}
@@ -101,12 +137,9 @@ export default function CoursesPage(Props: Props) {
             <SearchInput onChange={handleChangeSearchInput} />
             <SearchButton onClick={handleSearchButton} />
           </Stack>
-          {searchCount ? (
+          {searchCount > 0 && (
             <Table rows={rows} columns={["과목명", "학수번호", "학점"]} />
-          ) : (
-            <></>
           )}
-          {/* <MoreButton /> */}
         </Stack>
       </PageTemplate>
     </>
