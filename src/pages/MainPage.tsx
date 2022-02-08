8238c4466fd49350207d73dc3aeec1c317a794fc
junhwan <ujunhwan@gmail.com>
2022-02-09 02:21:14 +0900

    [feat] 수강 과목 확인하기 삭제

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -9,7 +9,6 @@ export default function MainPage(props: MainPageProps) {
   return (
     <PageTemplate>
       <Stack spacing={"0.5rem"}>
-        <MenuButton text="내가 수강한 과목 보기" href="/users/courses" />
         <MenuButton text="졸업요건 검사하기" href="/users/graduation" />
         <MenuButton text="등록된 과목 보기" href="/courses" />
       </Stack>
