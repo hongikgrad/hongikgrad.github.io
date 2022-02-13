89b1fcf0fd656d764c957f3a9acfee365d7c0916
junhwan <ujunhwan@gmail.com>
2022-02-14 02:44:08 +0900

    [feat] LoginPage: Navigate 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import Login from "../components/Login";
 import PageTemplate from "../components/PageTemplate";
+import { logoutUser } from "../modules/auth";
 
 export interface LoginPageProps {}
 
 export default function LoginPage(props: LoginPageProps) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(logoutUser());
+  }, []);
   return (
     <PageTemplate>
       <Login />
