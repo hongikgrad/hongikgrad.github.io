4a541d95ccd6d4c23f4c078582a7e71ca2d563a5
junhwan <ujunhwan@gmail.com>
2022-02-14 02:35:42 +0900

    [style] px

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -25,8 +25,8 @@ html {
 }
 
 body {
-  margin: 0;
-  padding: 0;
+  margin: 0px;
+  padding: 0px;
   font-family: Montserrat, Noto Sans KR, GothamSSmBook, -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Apple SD Gothic Neo", "Malgun Gothic", "맑은 고딕", 나눔고딕, "Nanum Gothic", "Noto Sans KR", "Noto Sans CJK KR", arial, 돋움, Dotum, Tahoma, Geneva, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
