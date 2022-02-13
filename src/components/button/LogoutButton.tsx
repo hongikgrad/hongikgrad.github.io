d8e61c058a748529f4aab421cf4694378af7a753
junhwan <ujunhwan@gmail.com>
2022-02-14 02:36:26 +0900

    [feat] logout button: add naviagte

--- src/components/button/LogoutButton.tsx
+++ src/components/button/LogoutButton.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { useDispatch } from "react-redux";
+import { Navigate, useNavigate } from "react-router";
 import { logoutUser } from "../../modules/auth";
 import Button from "./Button";
 import TextButton from "./TextButton";
 
 export default function LogoutButton(props: any) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handlerLogout = () => {
     dispatch(logoutUser());
+    navigate("/");
     // window.sessionStorage.setItem("login", "false");
   };
   return (
